627cf00224e42f2497504fdf8d1d1db49c613963
Prakash Premkumar <prakash.p.2292@gmail.com>
2024-03-13 11:31:07 +0530

    validate function added.

--- requests.go
+++ requests.go
@@ -1,8 +1,24 @@
 package database_clustermgt_model
 
+import (
+	"github.com/prakash-p-3121/errorlib"
+	"strings"
+)
+
 type DatabaseClusterCreateReq struct {
 	TableName   *string `json:"table-name"`
 	ShardIDList []int64 `json:"shard-id-list"`
+}
+
+func (req *DatabaseClusterCreateReq) Validate() errorlib.AppError {
+	if req.TableName == nil {
+		return errorlib.NewBadReqError("table_name-nil")
+	}
+	*req.TableName = strings.TrimSpace(*req.TableName)
+	if len(*req.TableName) == 0 {
+		return errorlib.NewBadReqError("table-name-empty")
+	}
+	return nil
 }
 
 type DatabaseShardCreateReq struct {
